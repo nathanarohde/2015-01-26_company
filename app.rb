2062e986ae4ccd00ecab9bb4472303fb86d49624
Monty Rohde & Shimran George <student@epicodus.com>
2015-01-26 15:36:32 -0800

    add final sinatra files. Pass initial functionality

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ get('/') do
 end
 
 post("/divisions") do
-  name =params['name']
+  name = params['name']
   divisions = Division.new({:name => name, :id => nil})
   divisions.save()
   @divisions = Division.all()
@@ -22,4 +22,17 @@ end
 get('/divisions/:id') do
   @division = Division.find(params['id'].to_i())
   erb(:divisions)
+end
+
+post('/employees') do
+  name = params['employee_name']
+  division_id = params['division_id'].to_i()
+  employee = Employee.create({:name => name, :division_id => division_id})
+  @division =Division.find(division_id)
+  erb(:divisions)
+end
+
+get('/employees/:id') do
+  @employee = Employee.find(params['id'].to_i())
+  erb(:employees)
 end
