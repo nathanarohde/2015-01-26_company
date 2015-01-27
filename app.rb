cab7febf437eec82df7cea74054683b3d8a29aee
Monty Rohde & Shimran George <student@epicodus.com>
2015-01-26 16:46:35 -0800

    Add Update/Delete functionality to employee page

--- app.rb
+++ app.rb
@@ -36,3 +36,24 @@ get('/employees/:id') do
   @employee = Employee.find(params['id'].to_i())
   erb(:employees)
 end
+
+get('/employees/:id/edit') do
+  @employee = Employee.find(params.fetch("id").to_i())
+  erb(:employees_edit)
+end
+
+patch '/employees/:id' do
+  name = params['employee_name']
+  @employee = Employee.find(params.fetch("id").to_i())
+  @employee.update({:name => name})
+  erb(:employees)
+end
+
+delete '/employees/:id' do
+  @employee = Employee.find(params.fetch("id").to_i())
+  @employee.delete
+  @divisions = Division.all()
+  #Removing changes table Division, so it needs to be reinitialized with .all()
+  #Delete is not cosmetic; it changes the id numbering within table, etc.
+  erb(:index)
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,19 @@ require('sinatra/activerecord')
 require('employee')
 require('division')
 require('pg')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Employee.all().each() do |employee|
+      employee.destroy()
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Divisions.all().each() do |division|
+      division.destroy()
+    end
+  end
+end
